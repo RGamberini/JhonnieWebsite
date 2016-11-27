807014e0a5d859abc4c39ee6146253121291c413
Rudy Gamberini <Rudy Gamberini>
2016-11-26 19:37:20 -0500

    Working different contents

--- jhonnieJS.js
+++ jhonnieJS.js
@@ -1,3 +1,24 @@
 $(document).ready(function () { // Once the document has loaded
-    $('#contact').show();
+    $('#contact').show(); // Show contact as our default screen
+    
+    $('#work').hide(); // Hide everything else
+    $('#statement').hide(); // Hide everything else
+    
+    $('#workButton').click(function () {
+        $('#contact').hide();
+        $('#statement').hide();
+        $('#work').show();
+    });
+    
+    $('#contactButton').click(function () {
+        $('#work').hide();
+        $('#statement').hide();
+        $('#contact').show();
+    });
+    
+    $('#statementButton').click(function () {
+        $('#work').hide();
+        $('#contact').hide();
+        $('#statement').show();
+    });
 });
