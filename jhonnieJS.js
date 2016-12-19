53cb84ded525868cebacffb6e85131bbf87e7b68
Rudy Gamberini <Rudy Gamberini>
2016-12-19 01:53:57 -0500

    Videos make everything better

--- jhonnieJS.js
+++ jhonnieJS.js
@@ -1,5 +1,5 @@
 $(document).ready(function () { // Once the document has loaded
-    $('#work').show(); // Show contact as our default screen
+    $('#work').show(); // Show wor as the default screen
     
     $('#contact').hide(); // Hide everything else
     $('#statement').hide(); // Hide everything else
@@ -20,5 +20,29 @@ $(document).ready(function () { // Once the document has loaded
         $('#work').hide();
         $('#contact').hide();
         $('#statement').show();
+    });
+    
+    var url = 'https://www.youtube.com/embed/{ID HERE}?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&playlist={ID HERE};';
+    $('#env').click(function () {
+        $(".backgroundImage").css("background-image", "url('env.png')");
+        $('#videoIframe').attr('src', '');
+    });
+    $('#warehouse').click(function () {
+        var id = '8DzrOQCEMTw';
+        var myUrl = url.replace('{ID HERE}', id).replace('{ID HERE}', id);
+        $('#videoIframe').attr('src', myUrl);
+        $(".backgroundImage").css("background-image", "url('')");
+    });
+    $('#visr').click(function () {
+        var id = 'nHtP2ydhAPI';
+        var myUrl = url.replace('{ID HERE}', id).replace('{ID HERE}', id);
+        $('#videoIframe').attr('src', myUrl);
+        $(".backgroundImage").css("background-image", "url('')");
+    });
+    $('#unity').click(function () {
+        var id = 'SIkB64oYPJU';
+        var myUrl = url.replace('{ID HERE}', id).replace('{ID HERE}', id);
+        $('#videoIframe').attr('src', myUrl);
+        $(".backgroundImage").css("background-image", "url('')");
     });
 });
